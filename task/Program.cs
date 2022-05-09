01399e99206defd17b57703d73506fc768a3ab25
AlexBurtsev <99988474+AlexBurtsev@users.noreply.github.com>
2022-05-09 17:55:47 +0300

    Добавили решение

---
+++ task/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] array1 = new string[5] {"2354", "hi", "hello", "world", "56"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
